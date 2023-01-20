8169326cbee144aa43afd64824f9e8faeea2cb08
Fidelis <fideliofidel9@gmail.com>
2023-01-20 11:00:34 +0300

    game-config Js

--- Project/JS/game.js
+++ Project/JS/game.js
@@ -75,8 +75,8 @@ choice1.appendChild(paragraph1Choice1);
 
 // Paragrapgh - 2 choice - 1
 const paragraph2Choice1 = document.createElement('p');
-paragraphChoice2Choice1.setAttribute('class', 'choice__text');
-paragraphChoice2Choice1.setAttribute('data-number', '1');
+paragraph2Choice1.setAttribute('class', 'choice__text');
+paragraph2Choice1.setAttribute('data-number', '1');
 
 choice1.appendChild(paragraph2Choice1);
 
@@ -87,7 +87,7 @@ quizDiv.appendChild(choice2);
 
 // Paragraph - 1 choice 2
 const paragraph1Choice2 = document.createElement('p');
-paragraph1Choice2.setAttribute('id', 'choice__prefix');
+paragraph1Choice2.setAttribute('class', 'choice__prefix');
 paragraph1Choice2.innerText = `B`;
 choice2.appendChild(paragraph1Choice2);
 
@@ -111,7 +111,7 @@ choice3.appendChild(paragraph1Choice3);
 
 // Paragraph -2 choice 3
 const paragraph2Choice3 = document.createElement('p');
-paragraph2Choice3.setAttribute('id', 'choice__text');
+paragraph2Choice3.setAttribute('class', 'choice__text');
 paragraph2Choice3.setAttribute('data-number', '3');
 
 choice3.appendChild(paragraph2Choice3);
@@ -123,13 +123,169 @@ quizDiv.appendChild(choice4);
 
 // Paragraph - 1 choice -4
 const paragraph1Choice4 = document.createElement('p');
-paragraph1Choice4.setAttribute('id', 'choice__prefix');
+paragraph1Choice4.setAttribute('class', 'choice__prefix');
 choice4.innerText = `D`;
 choice4.appendChild(paragraph1Choice4);
 
 // Paragraph -2 choice 4
 const paragraph2Choice4 = document.createElement('p');
-paragraph2Choice4.setAttribute('id', 'choice__text');
+paragraph2Choice4.setAttribute('class', 'choice__text');
 paragraph2Choice4.setAttribute('data-number', '4');
 
 choice4.appendChild(paragraph2Choice4);
+
+
+
+// Get the IDs
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice__text'));
+const progressText = document.getElementById('progressText');
+const scoreText = document.getElementById('score');
+const progressStatus = document.getElementById('progressStatus');
+const quiz = document.getElementById('quiz');
+
+
+// Object for Current Question
+let currentQuestion = {};
+// can't accept answer for delaying
+let acceptingAnswer = false;
+let score = 0;
+
+
+// Keep track of the score
+let questionCounter = 0;
+
+// Create an empty array to store questions from API
+let availableQuestions = [];
+
+let questions = [];
+
+// async await for fetching questions from open Trivia API DB
+async function getData() {
+    try {
+        const response = await fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple");
+    
+        const responsedData = await response.json();
+
+        // Loading questions
+        questions = responsedData.results.map((responsedQuestion) => {
+            const formattedQuestion = {
+                question: responsedQuestion.question,
+            };
+            // Choices
+            const answerChoices = [...responsedQuestion.incorrect_answers];
+
+            // Display the answer in a random position in answers array in UI
+            formattedQuestion.answer = Math.floor( Math.random()* 4 ) + 1;
+            answerChoices.splice(
+                formattedQuestion.answer - 1,
+                0,
+                responsedQuestion.correct_answer
+            );
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion['choice' + (index + 1)] = choice;
+            });
+            return formattedQuestion;
+        }); 
+        // Starting the quiz
+        startQuiz();
+    } catch (error) {
+        console.log(error);
+    }
+}
+// on Load
+getData();
+
+
+// Total points for each question
+const pointOnCorrectAnswer = 10;
+const totalQuestions = 10;
+
+// Initialize the question counter and score as 0
+startQuiz = () => {
+    questionCounter = 0;
+    score = 0;
+
+    // Copy questions from the API using spread(...)
+    availableQuestions = [...questions];
+
+    // To get New Questions
+    getNewQuestion();
+    quiz.classList.remove('hidden');
+    loader.classList.add('hidden');
+};
+
+getNewQuestion = () => {
+    // If all questions are used
+    if (availableQuestions.length === 0 || questionCounter >= totalQuestions) {
+        // save the most recent score to the variable score
+        localStorage.setItem('mostRecentScore', score);
+        // Redirect to End page
+        return window.location.assign('./end.html');
+    }
+
+    questionCounter++;
+
+    // Update question counter text
+    progressText.innerText = `Question ${questionCounter}/ ${totalQuestions}`;
+
+    // Update Progress bar
+    progressStatus.style.width = `${(questionCounter / totalQuestions)* 100 }%`;
+
+    // Display a random question
+    const questionIndex = Math.floor(Math.random()* availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerHTML = currentQuestion.question;
+
+    // get the choices
+
+    choices.forEach((choice) => {
+    // get the number from data-number
+        const number = choice.dataset['number'];
+        choice.innerHTML = currentQuestion[`choice ${number}`];
+    });
+
+     // Get new question
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswer = true;
+};
+
+// Each choice
+choices.forEach((choice) => {
+    // Event listener 
+    choice.addEventListener('click', (e) => {
+        if (!acceptingAnswer) return;
+        acceptingAnswer = false;
+
+        // Select answer and choice
+        const SelectedChoice = e.target
+        const SelectedAnswer = SelectedChoice.dataset['number'];
+        // If answer is correct add css class
+
+        const classToApply =
+        SelectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+        // If the answer is correct, increment the score
+        if (classToApply === 'correct') {
+            incrementScore(pointOnCorrectAnswer);
+        }
+
+
+        SelectedChoice.parentElement.classList.add(classToApply);
+        // Remove class after question has been answered
+        setTimeout(() => {
+            SelectedChoice.parentElement.classList.remove(classToApply);
+
+            // Get new question
+            getNewQuestion();
+        }, 1000);
+    });
+});
+
+// Increment the score
+incrementScore = (num) => {
+    score += num;
+    score.innerText = score;
+};
+
